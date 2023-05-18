3d5f39e2b8f7c5330cf0937c638289cdbf2e3a75
andstanon <stanescu_andrei13@yahoo.com>
2023-05-18 12:17:36 +0300

    first commit

---
+++ src/main/java/com/springframework/documentmanagementapp/DocumentManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.documentmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentManagementAppApplication.class, args);
+	}
+
+}
