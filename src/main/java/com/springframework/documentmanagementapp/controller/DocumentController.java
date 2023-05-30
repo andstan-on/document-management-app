d07d66f843d242d998b76c931eef19e465cf1543
andstanon <stanescu_andrei13@yahoo.com>
2023-05-30 23:43:29 +0300

    add tests for DocumentController

--- src/main/java/com/springframework/documentmanagementapp/controller/DocumentController.java
+++ src/main/java/com/springframework/documentmanagementapp/controller/DocumentController.java
@@ -3,6 +3,7 @@ package com.springframework.documentmanagementapp.controller;
 import com.springframework.documentmanagementapp.model.Document;
 import com.springframework.documentmanagementapp.services.DocumentService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -14,14 +15,16 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
+@RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/document")
 public class DocumentController {
 
-    @Autowired
-    private  DocumentService documentService;
+    public static final String DOCUMENT_PATH = "/api/v1/document";
+    public static final String DOCUMENT_PATH_ID = DOCUMENT_PATH + "/{documentId}";
 
-    @DeleteMapping("{documentId}")
+    private final DocumentService documentService;
+
+    @DeleteMapping(DOCUMENT_PATH_ID)
     public ResponseEntity deleteById(@PathVariable("documentId") UUID documentId){
         documentService.deleteById(documentId);
 
@@ -29,14 +32,14 @@ public class DocumentController {
 
     }
 
-    @PutMapping("{documentId}")
+    @PutMapping(DOCUMENT_PATH_ID)
     public ResponseEntity updateById(@PathVariable("documentId") UUID documentId, @RequestBody Document document){
         documentService.updateDocumentById(documentId, document);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping(DOCUMENT_PATH)
     public ResponseEntity handlePost(@RequestBody Document document){
         Document savedDocument = documentService.saveNewDocument(document);
 
@@ -46,12 +49,12 @@ public class DocumentController {
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping(DOCUMENT_PATH)
     public List<Document> listDocuments(){
         return documentService.listDocuments();
     }
 
-    @RequestMapping(value = "{documentId}", method = RequestMethod.GET)
+    @GetMapping(DOCUMENT_PATH_ID)
     public Document getDocumentById(@PathVariable("documentId") UUID documentId) {
 
         log.debug("Get Document by ID - in controller");
--- src/test/java/com/springframework/documentmanagementapp/controller/DocumentControllerTest.java
+++ src/test/java/com/springframework/documentmanagementapp/controller/DocumentControllerTest.java
@@ -1,22 +1,119 @@
 package com.springframework.documentmanagementapp.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springframework.documentmanagementapp.model.Document;
+import com.springframework.documentmanagementapp.services.DocumentService;
+import com.springframework.documentmanagementapp.services.DocumentServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@WebMvcTest(DocumentController.class)
 class DocumentControllerTest {
 
     @Autowired
-    DocumentController documentController;
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    DocumentService documentService;
+
+    DocumentServiceImpl documentServiceImpl;
+
+    @Captor
+    ArgumentCaptor<UUID> uuidArgumentCaptor;
+
+    @Captor
+    ArgumentCaptor<Document> documentArgumentCaptor;
+
+    @BeforeEach
+    void setUp() {
+        documentServiceImpl = new DocumentServiceImpl();
+    }
 
     @Test
-    void getDocumentById() {
+    void deleteById() throws Exception {
+        Document document = documentServiceImpl.listDocuments().get(0);
 
-        System.out.println(documentController.getDocumentById(UUID.randomUUID()));
+        mockMvc.perform(delete(DocumentController.DOCUMENT_PATH_ID, document.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        verify(documentService).deleteById(uuidArgumentCaptor.capture());
+        assertThat(document.getId()).isEqualTo(uuidArgumentCaptor.getValue());
+    }
+
+    @Test
+    void updateById() throws Exception {
+        Document document = documentServiceImpl.listDocuments().get(0);
+
+        mockMvc.perform(put(DocumentController.DOCUMENT_PATH_ID, document.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(document)))
+                .andExpect(status().isNoContent());
+
+        verify(documentService).updateDocumentById(any(UUID.class), any(Document.class));
+    }
+
+    @Test
+    void handlePost() throws  Exception {
+        Document document = documentServiceImpl.listDocuments().get(0);
+        document.setId(null);
+
+        given(documentService.saveNewDocument(any(Document.class))).willReturn(documentServiceImpl.listDocuments().get(1));
+
+        mockMvc.perform(post(DocumentController.DOCUMENT_PATH)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(document)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
+    }
+
+    @Test
+    void listDocuments() throws Exception {
+        given(documentService.listDocuments()).willReturn(documentServiceImpl.listDocuments());
+
+        mockMvc.perform(get(DocumentController.DOCUMENT_PATH)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(3)));
+    }
+
+    @Test
+    void getDocumentById() throws Exception {
+        Document testDocument = documentServiceImpl.listDocuments().get(0);
+
+        given(documentService.getDocumentById(testDocument.getId())).willReturn(testDocument);
+
+        mockMvc.perform(get(DocumentController.DOCUMENT_PATH_ID, testDocument.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(testDocument.getId().toString())))
+                .andExpect(jsonPath("$.vendorName", is(testDocument.getVendorName())));
     }
 }
